b683288479aa2fec4157a9eff89b428fbdaf13fc
Andrew Shore <andrewshore1414@gmail.com>
2019-03-15 09:59:58 -0600

    login

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 //const controllers = require("./controllers/thingController.js");
 app.use(epxress.static("public"));
 //app.use(express.json());
-const port = process.env.PORT || 5000;
