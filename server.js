ccfd3cadc995edde2195cc3ba47532cdad2120ef
Andrew Shore <andrewshore1414@gmail.com>
2019-03-15 11:51:53 -0600

    login

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 app.set("views", "views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 app.listen(port, function() {
     console.log(`Listening on port: ${port}`);
 });
