c4188052c86913337fee986f4ebab4cba918703c
Andrew Shore <andrewshore1414@gmail.com>
2019-03-15 09:30:48 -0600

    login

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const controllers = require("./controllers/thingController.js");
+//const controllers = require("./controllers/thingController.js");
 app.use(epxress.static("public"));
 app.use(express.json());
 
