bd82725da50633e622f2d7e9402f2cfeab922f7d
Andrew Shore <andrewshore1414@gmail.com>
2019-03-16 08:50:03 -0600

    login

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const { Pool } = require("pg");
+const connectionString = process.env.DATABASE_URL;
 
+const pool = new Pool({connectionString: connectionString});
 app.set("views", "views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -9,3 +12,23 @@ app.use(express.static("public"));
 app.listen(port, function() {
     console.log(`Listening on port: ${port}`);
 });
+
+app.get("loginUser",handleLogin);
+
+
+function handleLogin(request, response) {
+   const fname = request.query.fname;
+   const lname = request.query.lname;
+   const key = request.query.key;
+   var params = '{"fname": fname, "lname": lname, "key":key}';
+
+   checkDBuser(params, function(errr, result){
+
+   });
+}
+
+function checkDBuser(params, callback) {
+ const sql = "SELECT id, fname, lname FROM people where id = $1::int";
+ const reqObj = JSON.parse(params);
+ console.log(reqObj);
+}
