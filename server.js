0f551049410cf5a2ac2ed977bda0446879da8c53
Andrew Shore <andrewshore1414@gmail.com>
2019-03-16 09:46:37 -0600

    send to page

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function handleLogin(request, response) {
    checkDBuser(fname, function(errr, result){
      
      const context = result[0];
-     response.render("logedIN",context);
+     response.render("logedIN",JSON.stringify(context));
   //   response.render("logedIN",context);
    });
 }
