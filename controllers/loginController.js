482a828a5bf5368f166e706853d10567c827236f
Andrew Shore <andrewshore1414@gmail.com>
2019-03-21 16:44:31 -0600

    signup

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -22,16 +22,16 @@ function handleLogin(request, response) {
 }
 
 function handleSingup(req, res){
-    const fname = request.query.fname;
-    const lname = request.query.lname;
-    const key = request.query.key;
+    const fname = req.query.fname;
+    const lname = req.query.lname;
+    const key = req.query.key;
     const data = [fname,lname,key];
     lgmodel.handleSingup(data, function(err, result){
         // response.writeHead(302, {'Location' : '../login.html'});
         // response.end();
         console.log(results[0] + "id returned");
         if(results[0]) {
-          response.redirect("../login.html");
+          res.redirect("../login.html");
         }
     })
 }
