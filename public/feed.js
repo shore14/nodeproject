d675c292beed98c7313e17df133a7a5e19dadcbe
Andrew Shore <andrewshore1414@gmail.com>
2019-03-28 16:16:18 -0600

    genereate feed

--- public/feed.js
+++ public/feed.js
@@ -1,14 +1,14 @@
 function getStarted(){
-    const row = $();
+    const el = $();
     $(document).ready(function() {
    $.get("/startFeed", function(results) {
-    //   for (const key in results)
-    //   {
-    //       console.log("in the loop");
-    //       console.log(key + ": " + obj[key]);
-    //   }
+     rows = JSON.stringify(results);
+     for (row in results.rows){
+         el = el.add(<div>+row.title + row.body + row.type_def+</div>);
+     }
+     $("start").append(el);
     console.log("js" + JSON.stringify(results));
-    document.getElementById("start").innerHTML = JSON.stringify(results);
+    // document.getElementById("start").innerHTML = JSON.stringify(results);
    })
 });
 }
