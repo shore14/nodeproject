52700de6d6642573b5a837a221a5f377c1958651
Andrew Shore <andrewshore1414@gmail.com>
2019-03-28 15:19:48 -0600

    start

--- public/feed.js
+++ public/feed.js
@@ -1,12 +1,13 @@
 function getStarted(){
     const row = $();
+    $(document).ready(function() {
    $.get("/startFeed", function(results) {
     //   for (const key in results)
     //   {
     //       console.log("in the loop");
     //       console.log(key + ": " + obj[key]);
     //   }
-    
-   });
-   console.log("js" + JSON.stringify(results));
+    console.log("js" + JSON.stringify(results));
+   })
+});
 }
