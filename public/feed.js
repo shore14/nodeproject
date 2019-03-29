03c7263892696c8a7db68e0c82ab19f549f64b70
Andrew Shore <andrewshore1414@gmail.com>
2019-03-29 11:40:46 -0600

    genereate feed

--- public/feed.js
+++ public/feed.js
@@ -10,10 +10,7 @@ function getStarted(){
      for (const row in results){
             var title = row.title;
             var body = row.body;
-            var type_def = rows.type_def;
-            // const bagde = document.createElement('div');
-            // bagde.className = 'feedbox';
-            // bagde.innerHTML =
+            var type_def = row.type_def;
             badge +=  '<h2>' + title + '</h2></br>'
                 +'<div>' + body + '</div>';
          }
