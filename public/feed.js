b5c6eed82fdfd08023b533229a09374a145c0e4f
Andrew Shore <andrewshore1414@gmail.com>
2019-03-29 11:45:56 -0600

    genereate feed

--- public/feed.js
+++ public/feed.js
@@ -7,14 +7,17 @@ function getStarted(){
     var items = [];
     var badge = "";
     console.log(results);
-     for (const row in results){
-            var title = JSON.stringify(row.title);
-            var body = row.body;
-            var type_def = row.type_def;
-            badge +=  '<h2>' + title + '</h2></br>'
-                +'<div>' + body + '</div>';
-         }
-        document.getElementById('start').innerHTML = badge;
-   })
+    $.each(results, function(i, field){
+        $("start").append(field + " ");
+    });
+    //  for (const row in results){
+    //         var title = JSON.stringify(row.title);
+    //         var body = row.body;
+    //         var type_def = row.type_def;
+    //         badge +=  '<h2>' + title + '</h2></br>'
+    //             +'<div>' + body + '</div>';
+    //      }
+    //     document.getElementById('start').innerHTML = badge;
+   });
 });
 }
