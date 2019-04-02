76aa21b9436fc45ea60d684224d773ea7cadcde2
Andrew Shore <andrewshore1414@gmail.com>
2019-04-02 10:50:55 -0600

    feed attr

--- public/feed.js
+++ public/feed.js
@@ -1,14 +1,10 @@
 function getStarted(){
-    const el = $();
  $(document).ready(function() {
    $.getJSON("/startFeed", function(results) {
-    //  rows = JSON.stringify(results);
-    //  rows = JSON.parse(results);
     var items = [];
     var badge = "";
     console.log(results);
     $.each(results, function(i, field){
-        // $("start").append(field + " ");
         JSON.stringify(field);
         $(document.getElementById('start')).append("<div class='card'>" + field.title.toUpperCase() + " </br>" + field.body + "</div></br></br>");
     });
@@ -19,9 +15,9 @@ function getStarted(){
 function addCard(){
   $(document).ready(function() {
     var title = $("#title").val();
-    
-alert("Value of title is : " + title);
+    var body = $("#body").val();
     var value = $("form input[type='radio']:checked").val();
-alert("Value of Changed Radio is : " +value);
+
+    $(document.getElementById('start')).append("<div class='card'>" + title.toUpperCase() + " </br>" + body + "</div></br></br>");
   });
 }
